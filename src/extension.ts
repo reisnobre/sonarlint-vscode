44145a7956710a02b74e78bc3995e0e4c60e10b0
Kirill Knize <kirill.knize@sonarsource.com>
2020-08-26 15:21:27 +0200

    add cors whitelist

--- src/extension.ts
+++ src/extension.ts
@@ -48,7 +48,22 @@ let sonarlintOutput: VSCode.OutputChannel;
 let ruleDescriptionPanel: VSCode.WebviewPanel;
 let languageClient: SonarLintExtendedLanguageClient;
 const express = require('express');
+const cors = require('cors');
 const expressApp = express();
+
+let whitelist = ['http://localhost:9000']
+
+expressApp.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin 
+    if (!origin) return callback(null, true);
+    if (whitelist.indexOf(origin) === -1) {
+      var message = "The CORS policy for this origin doesn't allow access from the particular origin.";
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 export function logToSonarLintOutput(message) {
   if (sonarlintOutput) {
@@ -230,7 +245,7 @@ expressApp.get('/', function (req, res) {
       return VSCode.window.showTextDocument(document, options);
     })
     .then((result) => {
-        res.send('OK');
+      res.send('OK');
     },
       (e) => {
         res.send('Not OK');
